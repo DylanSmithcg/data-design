51d1fa5b02201fcce92f5de377cfb61d4bc1a44d
Dylan Smith <Dylansmithcg@gmail.com>
2020-03-13 07:43:55 -0700

    adding partial start on ddl

---
+++ sql/data-designddl.sql
@@ -0,0 +1,9 @@
+USE dsmith265;
+ALTER DATABASE dsmith265 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE category(
+	categoryId
+	categoryArtPieceId
+	categoryArtPieceStyleId
+	categoryArtistId
+)
