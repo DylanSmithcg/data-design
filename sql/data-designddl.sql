cd6132d2c23b5cb7ce56d2a6414d62187c6d08f0
Dylan Smith <Dylansmithcg@gmail.com>
2020-03-16 16:22:04 -0700

    done with phase 3?

--- sql/data-designddl.sql
+++ sql/data-designddl.sql
@@ -2,23 +2,33 @@ USE dsmith265;
 ALTER DATABASE dsmith265 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE category(
-	categoryId
-	categoryArtPieceId
-	categoryArtPieceStyleId
-	categoryArtistId
+	categoryId BINARY(16) NOT NULL,
+	categoryArtistId BINARY(16) NOT NULL,
+	categoryArtPieceId BINARY(16) NOT NULL,
+	categoryArtPieceStyleId VARCHAR(30) NOT NULL,
+	FOREIGN KEY(categoryArtistId) REFERENCES artist(artistId),
+	FOREIGN KEY(categoryArtPieceId) REFERENCES artPiece(artPieceId),
+	UNIQUE(categoryId),
+	PRIMARY KEY(categoryId)
 );
 
 CREATE TABLE artPiece(
-	artPieceId
-	artPieceStyleId
-	artPieceDescription
-	artPieceArtistId
-	artpieceArtististDescription
+	artPieceId BINARY(16) NOT NULL,
+	artPieceArtistDescription VARCHAR(255),
+	artPieceArtistId BINARY(16) NOT NULL,
+	artPieceCategoryId BINARY(16),
+	artPieceDescription VARCHAR(255) NOT NULL,
+	INDEX(artPieceCategoryId),
+	FOREIGN KEY(artPieceArtistId) REFERENCES artist(artistId),
+	FOREIGN KEY(artPieceCategoryId) REFERENCES category(categoryId),
+	PRIMARY KEY(artPieceId)
 );
 
 CREATE TABLE artist(
-	artistId
-	artistArtPieceId
+	artistId BINARY(16) NOT NULL,
+	artistArtPieceId BINARY(16) NOT NULL,
+	FOREIGN KEY(artistArtPieceId) REFERENCES artPiece(artPieceId),
+	PRIMARY KEY(artistId)
 );
 
 
